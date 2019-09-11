d3a56ff6b74b75d097cb74a21ef6ab70d6ab31bb
srisposi <54085792+srisposi@users.noreply.github.com>
2019-09-10 22:12:51 -0300

    Semana5

---
+++ Semana5/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="action_page.php">
+  Nombre:<br>
+  <input type="text" name="Nombre" value="">
+  <br>
+  Apellido:<br>
+  <input type="text" name="Apellido" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+</body>
+</html>
